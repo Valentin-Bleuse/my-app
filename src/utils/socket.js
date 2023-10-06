3f140f9d452dcb6d21e8afe39abb744db5986626
Valentin Bleuse <bleusevalentin@orange.fr>
2023-10-06 16:50:01 +0200

    push

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://iut-chat.karljustiniano.fr:1234", {
+export const socket = io("http://localhost:1234", {
+    // export const socket = io("https://iut-chat.karljustiniano.fr:1234", {
     autoConnect: false,
 });
-
 
 socket.onAny((event, ...args) => {
     console.log("event received", event, args);
